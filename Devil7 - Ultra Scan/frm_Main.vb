b29b93bf6e131eb6815dfb5744bb1295c29ddca4
Devil7DK <dineshthangavel47@gmail.com>
2019-06-23 10:42:08 +0530

    Add DevExpress Bonus Skins

--- Devil7 - Ultra Scan/frm_Main.vb
+++ Devil7 - Ultra Scan/frm_Main.vb
@@ -1,3 +1,10 @@
 ﻿Public Class frm_Main
 
+#Region "Constructor"
+    Public Sub New()
+        InitializeComponent()
+        DevExpress.UserSkins.BonusSkins.Register()
+    End Sub
+#End Region
+
 End Class
