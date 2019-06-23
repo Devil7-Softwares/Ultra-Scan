fdb9a28d6c1ca3ae1ed94311f413042b9006e50f
Devil7DK <dineshthangavel47@gmail.com>
2019-06-23 12:14:09 +0530

    Implement Initial Scanning & Page Management Functions

--- Devil7 - Ultra Scan/frm_Main.Designer.vb
+++ Devil7 - Ultra Scan/frm_Main.Designer.vb
@@ -19,58 +19,221 @@ Partial Class frm_Main
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frm_Main))
         Me.RibbonControl = New DevExpress.XtraBars.Ribbon.RibbonControl()
-        Me.RibbonPage1 = New DevExpress.XtraBars.Ribbon.RibbonPage()
-        Me.RibbonPageGroup1 = New DevExpress.XtraBars.Ribbon.RibbonPageGroup()
+        Me.cmb_Scanner = New DevExpress.XtraBars.BarEditItem()
+        Me.cmb_Scanner_Edit = New DevExpress.XtraEditors.Repository.RepositoryItemComboBox()
+        Me.btn_Scan = New DevExpress.XtraBars.BarButtonItem()
+        Me.sw_ShowUI = New DevExpress.XtraBars.BarToggleSwitchItem()
+        Me.btn_RemovePage = New DevExpress.XtraBars.BarButtonItem()
+        Me.btn_Page_MoveUp = New DevExpress.XtraBars.BarButtonItem()
+        Me.btn_Page_MoveDown = New DevExpress.XtraBars.BarButtonItem()
+        Me.rp_Home = New DevExpress.XtraBars.Ribbon.RibbonPage()
+        Me.rpg_Scanner = New DevExpress.XtraBars.Ribbon.RibbonPageGroup()
+        Me.rpg_Pages = New DevExpress.XtraBars.Ribbon.RibbonPageGroup()
         Me.RibbonStatusBar = New DevExpress.XtraBars.Ribbon.RibbonStatusBar()
+        Me.gallery_Pages = New DevExpress.XtraBars.Ribbon.GalleryControl()
+        Me.GalleryControlClient1 = New DevExpress.XtraBars.Ribbon.GalleryControlClient()
+        Me.container_Main = New DevExpress.XtraEditors.SplitContainerControl()
+        Me.pic_Preview = New DevExpress.XtraEditors.PictureEdit()
+        CType(Me.RibbonControl, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.cmb_Scanner_Edit, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.gallery_Pages, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.gallery_Pages.SuspendLayout()
+        CType(Me.container_Main, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.container_Main.SuspendLayout()
+        CType(Me.pic_Preview.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
-        ' 
-        ' RibbonControl
-        ' 
-        Me.RibbonControl.ApplicationIcon = Nothing
+        '
+        'RibbonControl
+        '
+        Me.RibbonControl.ExpandCollapseItem.Id = 0
+        Me.RibbonControl.Items.AddRange(New DevExpress.XtraBars.BarItem() {Me.RibbonControl.ExpandCollapseItem, Me.cmb_Scanner, Me.btn_Scan, Me.sw_ShowUI, Me.btn_RemovePage, Me.btn_Page_MoveUp, Me.btn_Page_MoveDown})
         Me.RibbonControl.Location = New System.Drawing.Point(0, 0)
+        Me.RibbonControl.MaxItemId = 9
         Me.RibbonControl.Name = "RibbonControl"
-        Me.RibbonControl.Pages.AddRange(New DevExpress.XtraBars.Ribbon.RibbonPage() {Me.RibbonPage1})
-        Me.RibbonControl.SelectedPage = Me.RibbonPage1
-        Me.RibbonControl.Size = New System.Drawing.Size(442, 141)
-        ' 
-        ' RibbonPage1
-        ' 
-        Me.RibbonPage1.Groups.AddRange(New DevExpress.XtraBars.Ribbon.RibbonPageGroup() {Me.RibbonPageGroup1})
-        Me.RibbonPage1.Name = "RibbonPage1"
-        Me.RibbonPage1.Text = "RibbonPage1"
-        ' 
-        ' RibbonPageGroup1
-        ' 
-        Me.RibbonPageGroup1.Name = "RibbonPageGroup1"
-        Me.RibbonPageGroup1.Text = "RibbonPageGroup1"
-        ' 
-        ' RibbonStatusBar
-        ' 
-        Me.RibbonStatusBar.Location = New System.Drawing.Point(0, 425)
+        Me.RibbonControl.Pages.AddRange(New DevExpress.XtraBars.Ribbon.RibbonPage() {Me.rp_Home})
+        Me.RibbonControl.RepositoryItems.AddRange(New DevExpress.XtraEditors.Repository.RepositoryItem() {Me.cmb_Scanner_Edit})
+        Me.RibbonControl.Size = New System.Drawing.Size(745, 143)
+        Me.RibbonControl.StatusBar = Me.RibbonStatusBar
+        '
+        'cmb_Scanner
+        '
+        Me.cmb_Scanner.Caption = "Scanner"
+        Me.cmb_Scanner.Edit = Me.cmb_Scanner_Edit
+        Me.cmb_Scanner.EditWidth = 150
+        Me.cmb_Scanner.Id = 1
+        Me.cmb_Scanner.Name = "cmb_Scanner"
+        '
+        'cmb_Scanner_Edit
+        '
+        Me.cmb_Scanner_Edit.AutoHeight = False
+        Me.cmb_Scanner_Edit.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
+        Me.cmb_Scanner_Edit.Name = "cmb_Scanner_Edit"
+        Me.cmb_Scanner_Edit.TextEditStyle = DevExpress.XtraEditors.Controls.TextEditStyles.DisableTextEditor
+        '
+        'btn_Scan
+        '
+        Me.btn_Scan.Caption = "Scan"
+        Me.btn_Scan.Id = 2
+        Me.btn_Scan.ImageOptions.SvgImage = CType(resources.GetObject("btn_Scan.ImageOptions.SvgImage"), DevExpress.Utils.Svg.SvgImage)
+        Me.btn_Scan.Name = "btn_Scan"
+        '
+        'sw_ShowUI
+        '
+        Me.sw_ShowUI.Caption = "Show Scanner UI"
+        Me.sw_ShowUI.Id = 5
+        Me.sw_ShowUI.Name = "sw_ShowUI"
+        '
+        'btn_RemovePage
+        '
+        Me.btn_RemovePage.Caption = "Remove"
+        Me.btn_RemovePage.Enabled = False
+        Me.btn_RemovePage.Id = 6
+        Me.btn_RemovePage.ImageOptions.SvgImage = CType(resources.GetObject("btn_RemovePage.ImageOptions.SvgImage"), DevExpress.Utils.Svg.SvgImage)
+        Me.btn_RemovePage.Name = "btn_RemovePage"
+        Me.btn_RemovePage.RibbonStyle = DevExpress.XtraBars.Ribbon.RibbonItemStyles.SmallWithText
+        '
+        'btn_Page_MoveUp
+        '
+        Me.btn_Page_MoveUp.Caption = "Move Up"
+        Me.btn_Page_MoveUp.Enabled = False
+        Me.btn_Page_MoveUp.Id = 7
+        Me.btn_Page_MoveUp.ImageOptions.SvgImage = CType(resources.GetObject("btn_Page_MoveUp.ImageOptions.SvgImage"), DevExpress.Utils.Svg.SvgImage)
+        Me.btn_Page_MoveUp.Name = "btn_Page_MoveUp"
+        Me.btn_Page_MoveUp.RibbonStyle = DevExpress.XtraBars.Ribbon.RibbonItemStyles.SmallWithText
+        '
+        'btn_Page_MoveDown
+        '
+        Me.btn_Page_MoveDown.Caption = "Move Down"
+        Me.btn_Page_MoveDown.Enabled = False
+        Me.btn_Page_MoveDown.Id = 8
+        Me.btn_Page_MoveDown.ImageOptions.SvgImage = CType(resources.GetObject("btn_Page_MoveDown.ImageOptions.SvgImage"), DevExpress.Utils.Svg.SvgImage)
+        Me.btn_Page_MoveDown.Name = "btn_Page_MoveDown"
+        Me.btn_Page_MoveDown.RibbonStyle = DevExpress.XtraBars.Ribbon.RibbonItemStyles.SmallWithText
+        '
+        'rp_Home
+        '
+        Me.rp_Home.Groups.AddRange(New DevExpress.XtraBars.Ribbon.RibbonPageGroup() {Me.rpg_Scanner, Me.rpg_Pages})
+        Me.rp_Home.Name = "rp_Home"
+        Me.rp_Home.Text = "Home"
+        '
+        'rpg_Scanner
+        '
+        Me.rpg_Scanner.ItemLinks.Add(Me.cmb_Scanner)
+        Me.rpg_Scanner.ItemLinks.Add(Me.sw_ShowUI)
+        Me.rpg_Scanner.Name = "rpg_Scanner"
+        Me.rpg_Scanner.ShowCaptionButton = False
+        Me.rpg_Scanner.Text = "Scanner"
+        '
+        'rpg_Pages
+        '
+        Me.rpg_Pages.ItemLinks.Add(Me.btn_Scan)
+        Me.rpg_Pages.ItemLinks.Add(Me.btn_Page_MoveUp, True)
+        Me.rpg_Pages.ItemLinks.Add(Me.btn_Page_MoveDown)
+        Me.rpg_Pages.ItemLinks.Add(Me.btn_RemovePage)
+        Me.rpg_Pages.Name = "rpg_Pages"
+        Me.rpg_Pages.ShowCaptionButton = False
+        Me.rpg_Pages.Text = "Pages"
+        '
+        'RibbonStatusBar
+        '
+        Me.RibbonStatusBar.Location = New System.Drawing.Point(0, 418)
         Me.RibbonStatusBar.Name = "RibbonStatusBar"
         Me.RibbonStatusBar.Ribbon = Me.RibbonControl
-        Me.RibbonStatusBar.Size = New System.Drawing.Size(442, 24)
-        ' 
-        ' frm_Main
-        ' 
-        Me.ClientSize = New System.Drawing.Size(442, 449)
+        Me.RibbonStatusBar.Size = New System.Drawing.Size(745, 31)
+        '
+        'gallery_Pages
+        '
+        Me.gallery_Pages.Controls.Add(Me.GalleryControlClient1)
+        Me.gallery_Pages.Dock = System.Windows.Forms.DockStyle.Fill
+        '
+        '
+        '
+        Me.gallery_Pages.Gallery.ImageSize = New System.Drawing.Size(124, 176)
+        Me.gallery_Pages.Gallery.ItemCheckMode = DevExpress.XtraBars.Ribbon.Gallery.ItemCheckMode.Multiple
+        Me.gallery_Pages.Gallery.ItemImageLayout = DevExpress.Utils.Drawing.ImageLayoutMode.ZoomInside
+        Me.gallery_Pages.Gallery.ShowItemText = True
+        Me.gallery_Pages.Location = New System.Drawing.Point(0, 0)
+        Me.gallery_Pages.Name = "gallery_Pages"
+        Me.gallery_Pages.Size = New System.Drawing.Size(503, 275)
+        Me.gallery_Pages.TabIndex = 2
+        Me.gallery_Pages.Text = "Pages"
+        '
+        'GalleryControlClient1
+        '
+        Me.GalleryControlClient1.GalleryControl = Me.gallery_Pages
+        Me.GalleryControlClient1.Location = New System.Drawing.Point(2, 2)
+        Me.GalleryControlClient1.Size = New System.Drawing.Size(482, 271)
+        '
+        'container_Main
+        '
+        Me.container_Main.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.container_Main.FixedPanel = DevExpress.XtraEditors.SplitFixedPanel.Panel2
+        Me.container_Main.Location = New System.Drawing.Point(0, 143)
+        Me.container_Main.Name = "container_Main"
+        Me.container_Main.Panel1.Controls.Add(Me.gallery_Pages)
+        Me.container_Main.Panel1.Text = "Panel1"
+        Me.container_Main.Panel2.Controls.Add(Me.pic_Preview)
+        Me.container_Main.Panel2.Text = "Panel2"
+        Me.container_Main.Size = New System.Drawing.Size(745, 275)
+        Me.container_Main.SplitterPosition = 237
+        Me.container_Main.TabIndex = 5
+        '
+        'pic_Preview
+        '
+        Me.pic_Preview.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.pic_Preview.Location = New System.Drawing.Point(0, 0)
+        Me.pic_Preview.MenuManager = Me.RibbonControl
+        Me.pic_Preview.Name = "pic_Preview"
+        Me.pic_Preview.Properties.AllowScrollOnMouseWheel = DevExpress.Utils.DefaultBoolean.[True]
+        Me.pic_Preview.Properties.AllowScrollViaMouseDrag = True
+        Me.pic_Preview.Properties.AllowZoomOnMouseWheel = DevExpress.Utils.DefaultBoolean.[True]
+        Me.pic_Preview.Properties.NullText = "Select Page to Preview"
+        Me.pic_Preview.Properties.ShowCameraMenuItem = DevExpress.XtraEditors.Controls.CameraMenuItemVisibility.[Auto]
+        Me.pic_Preview.Properties.ShowScrollBars = True
+        Me.pic_Preview.Properties.ShowZoomSubMenu = DevExpress.Utils.DefaultBoolean.[True]
+        Me.pic_Preview.Size = New System.Drawing.Size(237, 275)
+        Me.pic_Preview.TabIndex = 0
+        '
+        'frm_Main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(745, 449)
+        Me.Controls.Add(Me.container_Main)
         Me.Controls.Add(Me.RibbonStatusBar)
         Me.Controls.Add(Me.RibbonControl)
-
+        Me.Name = "frm_Main"
         Me.Ribbon = Me.RibbonControl
         Me.StatusBar = Me.RibbonStatusBar
         Me.Text = "frm_Main"
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        CType(Me.RibbonControl, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.cmb_Scanner_Edit, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.gallery_Pages, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.gallery_Pages.ResumeLayout(False)
+        CType(Me.container_Main, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.container_Main.ResumeLayout(False)
+        CType(Me.pic_Preview.Properties, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
-
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents RibbonControl As DevExpress.XtraBars.Ribbon.RibbonControl
-    Friend WithEvents RibbonPage1 As DevExpress.XtraBars.Ribbon.RibbonPage
-    Friend WithEvents RibbonPageGroup1 As DevExpress.XtraBars.Ribbon.RibbonPageGroup
+    Friend WithEvents rp_Home As DevExpress.XtraBars.Ribbon.RibbonPage
+    Friend WithEvents rpg_Scanner As DevExpress.XtraBars.Ribbon.RibbonPageGroup
     Friend WithEvents RibbonStatusBar As DevExpress.XtraBars.Ribbon.RibbonStatusBar
-
-
+    Friend WithEvents gallery_Pages As DevExpress.XtraBars.Ribbon.GalleryControl
+    Friend WithEvents GalleryControlClient1 As DevExpress.XtraBars.Ribbon.GalleryControlClient
+    Friend WithEvents cmb_Scanner As DevExpress.XtraBars.BarEditItem
+    Friend WithEvents cmb_Scanner_Edit As DevExpress.XtraEditors.Repository.RepositoryItemComboBox
+    Friend WithEvents rpg_Pages As DevExpress.XtraBars.Ribbon.RibbonPageGroup
+    Friend WithEvents btn_Scan As DevExpress.XtraBars.BarButtonItem
+    Friend WithEvents sw_ShowUI As DevExpress.XtraBars.BarToggleSwitchItem
+    Friend WithEvents container_Main As DevExpress.XtraEditors.SplitContainerControl
+    Friend WithEvents pic_Preview As DevExpress.XtraEditors.PictureEdit
+    Friend WithEvents btn_RemovePage As DevExpress.XtraBars.BarButtonItem
+    Friend WithEvents btn_Page_MoveUp As DevExpress.XtraBars.BarButtonItem
+    Friend WithEvents btn_Page_MoveDown As DevExpress.XtraBars.BarButtonItem
 End Class
--- Devil7 - Ultra Scan/frm_Main.vb
+++ Devil7 - Ultra Scan/frm_Main.vb
@@ -1,9 +1,216 @@
-﻿Public Class frm_Main
+﻿Imports Dynamsoft.TWAIN
+Imports Dynamsoft.TWAIN.Interface
+
+Public Class frm_Main : Implements IAcquireCallback
+
+#Region "Variables"
+    Private TWAINManager As TwainManager
+    Private ScanCount As Integer = 1
+    Private FitZoomPrecent As Double = -1
+#End Region
+
+#Region "Properties"
+    Private ReadOnly Property Pages As DevExpress.XtraBars.Ribbon.GalleryItemGroup
+        Get
+            If gallery_Pages.Gallery.Groups.Count = 0 Then gallery_Pages.Gallery.Groups.Add(New DevExpress.XtraBars.Ribbon.GalleryItemGroup With {.Caption = "Scanned Pages"})
+            Return gallery_Pages.Gallery.Groups(0)
+        End Get
+    End Property
+
+    Private ReadOnly Property ScannerIndex As Integer
+        Get
+            Return cmb_Scanner_Edit.Items.IndexOf(cmb_Scanner.EditValue)
+        End Get
+    End Property
+#End Region
 
 #Region "Constructor"
     Public Sub New()
         InitializeComponent()
         DevExpress.UserSkins.BonusSkins.Register()
+        TWAINManager = New TwainManager(Keys.DynamSoftKey)
+    End Sub
+#End Region
+
+#Region "Subs"
+    Function ShowMessage(ByVal Text As String, ByVal Caption As String, ByVal Buttons As MessageBoxButtons, ByVal Icon As MessageBoxIcon) As DialogResult
+        Return DevExpress.XtraEditors.XtraMessageBox.Show(Text, Caption, Buttons, Icon)
+    End Function
+
+    Private Sub RefreshScanners()
+        cmb_Scanner_Edit.Items.Clear()
+        For i As Integer = 0 To TWAINManager.SourceCount - 1
+            cmb_Scanner_Edit.Items.Add(TWAINManager.SourceNameItems(i))
+        Next
+        If cmb_Scanner_Edit.Items.Count > 0 And cmb_Scanner.EditValue Is Nothing Then cmb_Scanner.EditValue = cmb_Scanner_Edit.Items(0)
+    End Sub
+
+    Private Sub EnableControls()
+        If InvokeRequired Then
+            Invoke(Sub() EnableControls())
+        Else
+            rpg_Pages.Enabled = True
+            rpg_Scanner.Enabled = True
+        End If
+    End Sub
+
+    Private Sub DisableControls()
+        If InvokeRequired Then
+            Invoke(Sub() DisableControls())
+        Else
+            rpg_Pages.Enabled = False
+            rpg_Scanner.Enabled = False
+        End If
+    End Sub
+
+    Private Sub ZoomToFit()
+        Dim ZoomPercent As Double = Math.Min(100, Math.Min((pic_Preview.Width - 12) * 100 / pic_Preview.Image.Width,
+                               (pic_Preview.Height - 2) * 100 / pic_Preview.Image.Height))
+        pic_Preview.Properties.ZoomPercent = ZoomPercent
+        FitZoomPrecent = ZoomPercent
+    End Sub
+
+    Private Sub RetainFit()
+        If FitZoomPrecent <> -1 And pic_Preview.Properties.ZoomPercent = FitZoomPrecent Then
+            ZoomToFit()
+        End If
+    End Sub
+
+    Private Sub CheckForSelectedItems()
+        If InvokeRequired Then
+            Invoke(Sub() CheckForSelectedItems())
+        Else
+            Dim Count As Integer = gallery_Pages.Gallery.GetCheckedItemsCount
+
+            If Count = 0 Then
+                btn_Page_MoveDown.Enabled = False
+                btn_Page_MoveUp.Enabled = False
+                btn_RemovePage.Enabled = False
+            ElseIf Count = 1 Then
+                btn_Page_MoveDown.Enabled = (Pages.Items.IndexOf(gallery_Pages.Gallery.GetCheckedItems(0)) < Pages.Items.Count - 1)
+                btn_Page_MoveUp.Enabled = (Pages.Items.IndexOf(gallery_Pages.Gallery.GetCheckedItems(0)) > 0)
+                btn_RemovePage.Enabled = True
+            ElseIf Count > 1 Then
+                btn_Page_MoveDown.Enabled = False
+                btn_Page_MoveUp.Enabled = False
+                btn_RemovePage.Enabled = True
+            End If
+
+            If Count > 0 Then
+                Try
+                    pic_Preview.Image = gallery_Pages.Gallery.GetCheckedItems(Count - 1).Image
+                    ZoomToFit()
+                Catch ex As Exception
+
+                End Try
+            End If
+        End If
+    End Sub
+#End Region
+
+#Region "Interface"
+#Region "AcquireCallback"
+    Private Sub IAcquireCallback_OnPreAllTransfers() Implements IAcquireCallback.OnPreAllTransfers
+        DisableControls()
+    End Sub
+
+    Private Function IAcquireCallback_OnPreTransfer() As Boolean Implements IAcquireCallback.OnPreTransfer
+        Return True
+    End Function
+
+    Private Function IAcquireCallback_OnPostTransfer(bit As Bitmap) As Boolean Implements IAcquireCallback.OnPostTransfer
+        Pages.Items.Add(New DevExpress.XtraBars.Ribbon.GalleryItem(bit, String.Format("Scan {0:000}", ScanCount), ""))
+        Me.ScanCount += 1
+        CheckForSelectedItems()
+        Return True
+    End Function
+
+    Private Sub IAcquireCallback_OnPostAllTransfers() Implements IAcquireCallback.OnPostAllTransfers
+        EnableControls()
+    End Sub
+
+    Private Sub IAcquireCallback_OnSourceUIClose() Implements IAcquireCallback.OnSourceUIClose
+        EnableControls()
+    End Sub
+
+    Private Sub IAcquireCallback_OnTransferCancelled() Implements IAcquireCallback.OnTransferCancelled
+        ShowMessage("Scan cancelled by the user!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+        EnableControls()
+    End Sub
+
+    Private Sub IAcquireCallback_OnTransferError() Implements IAcquireCallback.OnTransferError
+        ShowMessage("Scan failed!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+    End Sub
+#End Region
+#End Region
+
+#Region "Form Events"
+    Private Sub frm_Main_Load(sender As Object, e As EventArgs) Handles Me.Load
+        TWAINManager.OpenSourceManager()
+        RefreshScanners()
+    End Sub
+
+    Private Sub frm_Main_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+        TWAINManager.CloseSource()
+        TWAINManager.CloseSourceManager()
+    End Sub
+#End Region
+
+#Region "Button Events"
+    Private Sub btn_Scan_ItemClick(sender As Object, e As DevExpress.XtraBars.ItemClickEventArgs) Handles btn_Scan.ItemClick
+        TWAINManager.SelectSourceByIndex(ScannerIndex)
+        TWAINManager.IfShowUI = sw_ShowUI.Checked
+        TWAINManager.OpenSource()
+        TWAINManager.AcquireImage(Me)
+    End Sub
+
+    Private Sub btn_Page_MoveUp_ItemClick(sender As Object, e As DevExpress.XtraBars.ItemClickEventArgs) Handles btn_Page_MoveUp.ItemClick
+        If Pages.GetCheckedItemsCount = 1 Then
+            Dim SelectedItem As DevExpress.XtraBars.Ribbon.GalleryItem = Pages.GetCheckedItems(0)
+            Dim NewIndex As Integer = Pages.Items.IndexOf(SelectedItem) - 1
+            If NewIndex >= 0 Then
+                Pages.Items.Remove(SelectedItem)
+                Pages.Items.Insert(NewIndex, SelectedItem)
+                CheckForSelectedItems()
+            End If
+        End If
+    End Sub
+
+    Private Sub btn_Page_MoveDown_ItemClick(sender As Object, e As DevExpress.XtraBars.ItemClickEventArgs) Handles btn_Page_MoveDown.ItemClick
+        If Pages.GetCheckedItemsCount = 1 Then
+            Dim SelectedItem As DevExpress.XtraBars.Ribbon.GalleryItem = Pages.GetCheckedItems(0)
+            Dim NewIndex As Integer = Pages.Items.IndexOf(SelectedItem) + 1
+            If NewIndex <= Pages.Items.Count - 1 Then
+                Pages.Items.Remove(SelectedItem)
+                Pages.Items.Insert(NewIndex, SelectedItem)
+                CheckForSelectedItems()
+            End If
+        End If
+    End Sub
+
+    Private Sub btn_RemovePage_ItemClick(sender As Object, e As DevExpress.XtraBars.ItemClickEventArgs) Handles btn_RemovePage.ItemClick
+        If Pages.GetCheckedItemsCount > 0 Then
+            If ShowMessage("Do you want to remove selected pages? This action cannot be reversed!", "Sure?", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation) = DialogResult.Yes Then
+                Dim Items2Remove As List(Of DevExpress.XtraBars.Ribbon.GalleryItem) = Pages.GetCheckedItems
+                For Each i As DevExpress.XtraBars.Ribbon.GalleryItem In Items2Remove
+                    Pages.Items.Remove(i)
+                Next
+            End If
+        End If
+    End Sub
+#End Region
+
+#Region "Other Events"
+    Private Sub gallery_Pages_Gallery_ItemCheckedChanged(sender As Object, e As DevExpress.XtraBars.Ribbon.GalleryItemEventArgs) Handles gallery_Pages.Gallery.ItemCheckedChanged
+        CheckForSelectedItems()
+    End Sub
+
+    Private Sub container_Main_SplitterMoving(sender As Object, e As DevExpress.XtraEditors.SplitMovingEventArgs) Handles container_Main.SplitterMoving
+        RetainFit()
+    End Sub
+
+    Private Sub container_Main_SizeChanged(sender As Object, e As EventArgs) Handles container_Main.SizeChanged
+        RetainFit()
     End Sub
 #End Region
 
